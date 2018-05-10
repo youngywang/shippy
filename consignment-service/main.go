e86bb8357a870ddd5d199b41c6e2698df5420c02
wuYin <wuYinPost@gmail.com>
2018-05-10 15:51:45 +0800

    完成 consignment-service 微服务的服务端

---
+++ consignment-service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	pb "shipper/consignment-service/proto/consignment"
+	"context"
+	"net"
+	"log"
+	"google.golang.org/grpc"
+)
+
+const (
+	PORT = ":50051"
+)
+
+// 我们自己的仓库
+type IRepository interface {
+	Create(consignment *pb.Consignment) (*pb.Consignment, error)
+}
+
+// 模拟存放多批货物的数据库
+// 实现 IRepository 接口
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.consignments = append(repo.consignments, consignment)
+	return consignment, nil
+}
+
+// 定义微服务
+type service struct {
+	repo Repository
+}
+
+// 实现 consignment.pb.go 中的 ShippingServiceServer 接口
+// 使 service 作为 gRPC 的服务端
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	// 接收承运的货物
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+	resp := &pb.Response{Created: true, Consignment: consignment}
+	return resp, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	repo := Repository{}
+	pb.RegisterShippingServiceServer(server, &service{repo})
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
