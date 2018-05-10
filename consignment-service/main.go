1eb89e9e2ca7038b819c93146e30f77e1d821fde
wuYin <wuYinPost@gmail.com>
2018-05-10 16:57:16 +0800

    服务端实现查看所有货物信息的 RPC

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -12,13 +12,17 @@ const (
 	PORT = ":50051"
 )
 
-// 我们自己的仓库
+//
+// 仓库接口
+//
 type IRepository interface {
-	Create(consignment *pb.Consignment) (*pb.Consignment, error)
+	Create(consignment *pb.Consignment) (*pb.Consignment, error) // 存放新货物
+	GetAll() []*pb.Consignment                                   // 获取仓库中所有的货物
 }
 
-// 模拟存放多批货物的数据库
-// 实现 IRepository 接口
+//
+// 我们存放多批货物的仓库，实现了 IRepository 接口
+//
 type Repository struct {
 	consignments []*pb.Consignment
 }
@@ -28,13 +32,22 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	return consignment, nil
 }
 
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+//
 // 定义微服务
+//
 type service struct {
 	repo Repository
 }
 
+//
 // 实现 consignment.pb.go 中的 ShippingServiceServer 接口
 // 使 service 作为 gRPC 的服务端
+//
+// 托运新的货物
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
 	// 接收承运的货物
 	consignment, err := s.repo.Create(req)
@@ -42,6 +55,13 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 		return nil, err
 	}
 	resp := &pb.Response{Created: true, Consignment: consignment}
+	return resp, nil
+}
+
+// 获取目前所有托运的货物
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	allConsignments := s.repo.GetAll()
+	resp := &pb.Response{Consignments: allConsignments}
 	return resp, nil
 }
 
