e304ab711b88b8c0ba1947a88926d3d4271b29b5
wuYin <wuYinPost@gmail.com>
2018-05-28 11:43:46 +0800

    添加认证函数

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	pb "shippy/consignment-service/proto/consignment"
 	vesselPb "shippy/vessel-service/proto/vessel"
+	userPb "shippy/user-service/proto/user"
 	"log"
 	"github.com/micro/go-micro"
 	"os"
+	"github.com/micro/go-micro/server"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/client"
+	"context"
+	"errors"
 )
 
 const (
-	DEFAULT_HOST = "localhost:27017"
+	DEFAULT_HOST = "127.0.0.1:27017"
 )
 
 
@@ -27,20 +33,55 @@ func main() {
 		log.Fatalf("create session error: %v\n", err)
 	}
 
-	server := micro.NewService(
+	srv := micro.NewService(
 		// 必须和 consignment.proto 中的 package 一致
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
+		micro.WrapHandler(AuthWrapper),
 	)
 
 	// 解析命令行参数
-	server.Init()
+	srv.Init()
 	// 作为 vessel-service 的客户端
-	vClient := vesselPb.NewVesselServiceClient("go.micro.srv.vessel", server.Client())
+	vClient := vesselPb.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
 	// 将 server 作为微服务的服务端
-	pb.RegisterShippingServiceHandler(server.Server(), &handler{session, vClient})
+	pb.RegisterShippingServiceHandler(srv.Server(), &handler{session, vClient})
 
-	if err := server.Run(); err != nil {
+	if err := srv.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+//
+//  AuthWrapper 是一个高阶函数，入参是 ”下一步“ 函数，出参是认证函数
+// 在返回的函数内部处理完认证逻辑后，再手动调用 fn() 进行下一步处理
+// token 是从 consignment-ci 上下文中取出的，再调用 user-service 将其做验证
+// 认证通过则 fn() 继续执行，否则报错
+//
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, resp interface{}) error {
+		// consignment-service 独立测试时不进行认证
+		if os.Getenv("DISABLE_AUTH") == "true" {
+			return fn(ctx, req, resp)
+		}
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in request")
+		}
+
+		// Note this is now uppercase (not entirely sure why this is...)
+		token := meta["Token"]
+
+		// Auth here
+		authClient := userPb.NewUserServiceClient("go.micro.srv.user", client.DefaultClient)
+		authResp, err := authClient.ValidateToken(context.Background(), &userPb.Token{
+			Token: token,
+		})
+		log.Println("Auth Resp:", authResp)
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, resp)
+		return err
 	}
 }
